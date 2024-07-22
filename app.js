48d57d29ec5b2d73445c66cbe6da1905e6941214
kmal808 <kjmalley7@gmail.com>
2024-07-22 07:30:06 -1000

    feat: adding edit and delete operatoins/routes to backend

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import createHomepageTemplate from './views/index.js'
 import createListTemplate from './views/list.js'
+import createBookmarkTemplate from './views/bookmark.js'
+import createEditFormTemplate from './views/edit.js'
+
 import BOOKMARKS_DATA from './data/data.js'
 
 // create app
@@ -16,18 +19,57 @@ app.get('/', (req, res) => {
 })
 
 app.get('/bookmarks', (req, res) => {
-	res.send(createListTemplate())
+	res.send(createListTemplate(BOOKMARKS_DATA))
 })
 
 app.post('/bookmarks', (req, res) => {
 	const { title, url, label } = req.body
-	const id = Math.random().toString(36).substring(2, 15)
+	const id = Math.random().toString()
 
 	BOOKMARKS_DATA.push({ id, title, url, label })
-	res.send(createListTemplate())
+	res.redirect('/bookmarks/' + id)
+})
+
+app.get('bookmarks/:id', (req, res) => {
+	const { id } = req.params
+
+	const bookmark = BOOKMARKS_DATA.find((bookmark) => bookmark.id === id)
+	res.send(createBookmarkTemplate(bookmark))
+})
+
+app.delete('bookmarks/:id', (req, res) => {
+	const idx = BOOKMARKS_DATA.findIndex(
+		(bookmark) => bookmark.id === req.params.id
+	)
+	BOOKMARKS_DATA.splice(idx, 1)
+	res.send()
+})
+
+app.put('bookmarks/:id', (req, res) => {
+	const { title, url, label } = req.body
+	const { id } = req.params
+	const newBookmark = { id, title, url, label }
+	const idx = BOOKMARKS_DATA.findIndex((bookmark) => bookmark.id === id)
+	BOOKMARKS_DATA[idx] = newBookmark
+	res.send(createBookmarkTemplate(newBookmark))
+})
+
+app.get('bookmarks/edit/:id', (req, res) => {
+	const bookmark = BOOKMARKS_DATA.find(
+		(bookmark) => bookmark.id === req.params.id
+	)
+	res.send(createEditFormTemplate(bookmark))
+})
+
+app.post('bookmarks/search', (req, res) => {
+	const text = req.body.search.toLowerCase()
+	const bookmarks = BOOKMARKS_DATA.filter((bookmark) =>
+		bookmark.title.toLowerCase().includes(text)
+	)
+	res.send(createListTemplate(bookmarks))
 })
 
 // listen to port
 app.listen(3000, () => {
-	console.log('App listening on port 3000')
+	console.log('App is running 🏃‍➡️ on port 3000, better catch 🏈 it')
 })
